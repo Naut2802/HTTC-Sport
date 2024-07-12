5d7b284956bc3294a32d5d05cbc7cf0f23865cc0
Naut2802 <willbin012@gmail.com>
2024-07-13 04:06:52 +0700

    user

--- FE/httc-sport/src/components/Authenticate/index.jsx
+++ FE/httc-sport/src/components/Authenticate/index.jsx
@@ -3,7 +3,6 @@ import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { setToken } from '~/services/localStorageService';
-import { toast } from 'react-toastify';
 
 export default function Authenticate() {
     const navigate = useNavigate();

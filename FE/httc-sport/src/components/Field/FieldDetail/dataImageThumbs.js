c50f1432e24f2622d3e2e826822d0eef54d5fd4a
Naut2802 <willbin012@gmail.com>
2024-07-19 21:27:49 +0700

    .

--- FE/httc-sport/src/components/Field/FieldDetail/dataImageThumbs.js
+++
@@ -1,6 +0,0 @@
-export const sanImages = [
-  require("../../Images/sanquynhnhu/anh_san_1_1.png"),
-  require("../../Images/sanquynhnhu/anh_san_1_2.png"),
-  require("../../Images/sanquynhnhu/anh_san_1_3.png"),
-  require("../../Images/sanquynhnhu/anh_san_1_4.png"),
-];

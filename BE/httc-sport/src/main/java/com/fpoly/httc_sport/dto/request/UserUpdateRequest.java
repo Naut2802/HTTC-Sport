faac2e9855cc708d03797d7c27caf2683a1f6079
Naut2802 <willbin012@gmail.com>
2024-07-29 11:44:37 +0700

    update validation

--- BE/httc-sport/src/main/java/com/fpoly/httc_sport/dto/request/UserUpdateRequest.java
+++ BE/httc-sport/src/main/java/com/fpoly/httc_sport/dto/request/UserUpdateRequest.java
@@ -2,6 +2,7 @@ package com.fpoly.httc_sport.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
@@ -16,6 +17,7 @@ import lombok.experimental.FieldDefaults;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserUpdateRequest{
 	@NotNull(message = "EMAIL_NULL")
+	@NotBlank(message = "EMAIL_NULL")
 	@Email(message = "EMAIL_INVALID")
 	String email;
 	String firstName;

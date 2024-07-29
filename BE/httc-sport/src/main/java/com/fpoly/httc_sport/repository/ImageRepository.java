bcdada5adbfa6d6640c3ceabac4f4b157d2fe9d0
Naut2802 <willbin012@gmail.com>
2024-07-29 14:43:14 +0700

    update pitch feature: fixed update pitch, remove image from pitch

--- BE/httc-sport/src/main/java/com/fpoly/httc_sport/repository/ImageRepository.java
+++ BE/httc-sport/src/main/java/com/fpoly/httc_sport/repository/ImageRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.fpoly.httc_sport.entity.Image;
 
 public interface ImageRepository extends JpaRepository<Image, Long> {
-	List<Image> findAllByPublicIdIn(Iterable<String> publicIds);
+	List<Image> findByPublicIdIn(Iterable<String> publicIds);
 	Optional<Image> findByPublicId(String publicId);
 	void deleteByPublicId(String publicId);
 	boolean existsByPublicId(String publicId);

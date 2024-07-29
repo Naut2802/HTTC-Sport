ca55fdfd5ec772ad68ec8d7793ea58f7a3272727
Ton Le <leton1629@gmail.com>
2024-07-29 18:00:55 +0700

    test 3

--- FE/httc-sport/src/components/Admin/AddPitch/FormAddPitch/index.jsx
+++ FE/httc-sport/src/components/Admin/AddPitch/FormAddPitch/index.jsx
@@ -37,7 +37,6 @@ const ValidationTextField = styled(TextField)({
 export default function FormAddPitch({ selectedPitch }) {
     const { register, handleSubmit, control, setValue, reset } = useForm();
 
-    //test
     // const navigate = useNavigate();
     const [dataCity, setDataCity] = useState([]);
     const [districts, setDistricts] = useState([]);

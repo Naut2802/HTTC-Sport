ff8c17121e9067f90b03824a2bf84584993605a9
Naut2802 <willbin012@gmail.com>
2024-07-29 13:02:44 +0700

    update pitch feature: pagination

--- BE/httc-sport/src/main/java/com/fpoly/httc_sport/repository/PitchRepository.java
+++ BE/httc-sport/src/main/java/com/fpoly/httc_sport/repository/PitchRepository.java
@@ -2,12 +2,14 @@ package com.fpoly.httc_sport.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.fpoly.httc_sport.entity.Pitch;
 
 public interface PitchRepository extends JpaRepository<Pitch, Integer> {
-	List<Pitch> findAllByIsEnabledTrue();
+	Page<Pitch> findAllByIsEnabledTrue(Pageable pageable);
 	List<Pitch> findByPitchNameContainingAndIsEnabledTrue(String pitchName);
 	List<Pitch> findByTypeEqualsAndIsEnabledTrue(String type);
 }

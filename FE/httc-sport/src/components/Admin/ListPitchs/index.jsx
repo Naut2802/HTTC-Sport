32d072b466a003326bcc892a2b143b12560dae3c
Le Minh Hoang <leminhhoang241299@gmail.com>
2024-08-02 18:06:25 +0700

    test

--- FE/httc-sport/src/components/Admin/ListPitchs/index.jsx
+++ FE/httc-sport/src/components/Admin/ListPitchs/index.jsx
@@ -1,17 +1,17 @@
-import CreateIcon from '@mui/icons-material/Create';
-import DeleteIcon from '@mui/icons-material/Delete';
+import React, { useEffect, useState } from 'react';
 import { Breadcrumbs, Button, Tooltip, Typography } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
-import React, { useEffect, useState } from 'react';
+import DeleteIcon from '@mui/icons-material/Delete';
+import CreateIcon from '@mui/icons-material/Create';
+import { handleGetPitches, handleDeletePitch } from '~/apis';
 import { toast } from 'react-toastify';
-import { handleGetPitches } from '~/apis';
 
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-export default function ListFieldAdmin({ onRowClick }) {
+export default function ListPitchs({ onRowClick }) {
     const [pitch, setPitch] = useState([]);
-    const navigate = useNavigate();
 
+    //Dữ liệu call Api
     useEffect(() => {
         const fetchData = async () => {
             try {
@@ -35,17 +35,68 @@ export default function ListFieldAdmin({ onRowClick }) {
         window.location.href = '/admin/them-san';
     };
 
+    //Xóa sân
+    const handleDelClick = async (row) => {
+        const pitchId = row.id;
+        if (pitchId === undefined) {
+            toast.error('Không thấy ID');
+            return;
+        }
+        try {
+            const response = await handleDeletePitch(pitchId);
+            console.log('API response:', response);
+
+            if (response && response.data) {
+                toast.success(response.data.message);
+                setPitch((prevRows) => prevRows.filter((item) => item.id !== pitchId));
+            } else {
+                toast.error('Xóa sân thất bại!');
+            }
+        } catch (error) {
+            console.error('Failed to delete pitch:', error);
+            toast.error('Xóa sân thất bại!');
+        }
+    };
+
+    //truyền dữ liệu vào column qua field = name
     const columns = [
         { field: 'pitchName', headerName: 'Tên Sân', width: 150 },
-        { field: 'images', headerName: 'Hình Ảnh', width: 150 },
-        { field: 'price', headerName: 'Giá', width: 80 },
-        { field: 'description', headerName: 'Mô Tả', width: 150 },
-        { field: 'address', headerName: 'Địa Chỉ', width: 570 },
+        {
+            field: 'images',
+            headerName: 'Hình Ảnh',
+            width: 220,
+            renderCell: (params) => {
+                if (params.value && params.value.length > 0) {
+                    return (
+                        <div
+                            style={{
+                                height: '100%',
+                                display: 'flex',
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                                margin: '10px',
+                            }}
+                        >
+                            <img
+                                src={params.value[0].url}
+                                alt="Hình Ảnh"
+                                style={{ height: '190px', width: '190px', objectFit: 'contain' }}
+                            />
+                        </div>
+                    );
+                } else {
+                    return <span>No Image</span>;
+                }
+            },
+        },
+        { field: 'price', headerName: 'Giá', width: 120 },
+        { field: 'description', headerName: 'Mô Tả', width: 180 },
+        { field: 'address', headerName: 'Địa Chỉ', width: 460 },
         {
             field: 'other',
             headerName: 'Khác',
             sortable: false,
-            width: 100,
+            width: 150,
             renderCell: (params) => (
                 <>
                     <Tooltip title="Chỉnh Sửa" variant="solid">
@@ -55,7 +106,7 @@ export default function ListFieldAdmin({ onRowClick }) {
                     </Tooltip>
                     <Tooltip title="Xóa" variant="solid">
                         <Button sx={{ color: 'red' }}>
-                            <DeleteIcon />
+                            <DeleteIcon onClick={() => handleDelClick(params.row)} />
                         </Button>
                     </Tooltip>
                 </>
@@ -80,6 +131,7 @@ export default function ListFieldAdmin({ onRowClick }) {
                 pageSize={10}
                 rowsPerPageOptions={[5, 10, 20, 50, 100]}
                 getRowId={(row) => row.id}
+                rowHeight={150}
             />
         </div>
     );

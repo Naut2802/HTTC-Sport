6bddaf6120227e3e34b8a407d82b78ff97586ff9
Naut2802 <willbin012@gmail.com>
2024-08-04 17:37:40 +0700

    update feature: pitch response

--- BE/httc-sport/src/main/java/com/fpoly/httc_sport/mapper/RoleMapper.java
+++ BE/httc-sport/src/main/java/com/fpoly/httc_sport/mapper/RoleMapper.java
@@ -1,12 +1,8 @@
 package com.fpoly.httc_sport.mapper;
 
-import com.fpoly.httc_sport.dto.request.RoleRequest;
-import com.fpoly.httc_sport.dto.request.RoleUpdateRequest;
 import com.fpoly.httc_sport.dto.response.RoleResponse;
 import com.fpoly.httc_sport.entity.Role;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface RoleMapper {

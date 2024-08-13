02011d00c6263912ff1c6401dd9fee5dd87ecda3
Naut2802 <willbin012@gmail.com>
2024-08-14 04:12:35 +0700

    update redirect uri

--- FE/httc-sport/src/utils/constants.js
+++ FE/httc-sport/src/utils/constants.js
@@ -2,12 +2,12 @@ export const API_ROOT = 'https://be.httc-sport.online';
 
 export const OAuthConfigGoogle = {
     clientId: '390468462355-605umotentpl4jqu1pqdh57f6351lsv6.apps.googleusercontent.com',
-    redirectUri: 'http://localhost:3000/authenticate/google',
+    redirectUri: 'https://httc-sport.online/authenticate/google',
     authUri: 'https://accounts.google.com/o/oauth2/auth',
 };
 
 export const OAuthConfigFacebook = {
     clientId: '1454809475067563',
-    redirectUri: 'http://localhost:3000/authenticate/facebook',
+    redirectUri: 'https://httc-sport.online/authenticate/facebook',
     authUri: 'https://www.facebook.com/v20.0/dialog/oauth',
 };

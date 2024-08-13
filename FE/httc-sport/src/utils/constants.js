a1b9eeb57868c0c766ad60b46756c7e17097663f
Naut2802 <willbin012@gmail.com>
2024-08-14 05:59:07 +0700

    update

--- FE/httc-sport/src/utils/constants.js
+++ FE/httc-sport/src/utils/constants.js
@@ -2,12 +2,12 @@ export const API_ROOT = 'https://be.httc-sport.online';
 
 export const OAuthConfigGoogle = {
     clientId: '390468462355-605umotentpl4jqu1pqdh57f6351lsv6.apps.googleusercontent.com',
-    redirectUri: 'https://httc-sport.online/#/authenticate/google',
+    redirectUri: 'https://httc-sport.online/authenticate/google',
     authUri: 'https://accounts.google.com/o/oauth2/auth',
 };
 
 export const OAuthConfigFacebook = {
     clientId: '1454809475067563',
-    redirectUri: 'https://httc-sport.online/#/authenticate/facebook',
+    redirectUri: 'https://httc-sport.online/authenticate/facebook',
     authUri: 'https://www.facebook.com/v20.0/dialog/oauth',
 };

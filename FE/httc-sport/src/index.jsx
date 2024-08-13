e590012413bdda24d05e912435ce32f2cd8a10c5
Naut2802 <willbin012@gmail.com>
2024-08-14 05:46:32 +0700

    update

--- FE/httc-sport/src/index.jsx
+++ FE/httc-sport/src/index.jsx
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { createHashRouter,RouterProvider } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -12,15 +12,23 @@ import App from './App';
 import GlobalStyles from './components/GlobalStyles';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <BrowserRouter>
+const router = createHashRouter([
+    {
+      path: "/*",
+      element: <App />,
+    }
+  ]);
+  
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <RouterProvider router={router}>
         <LocalizationProvider dateAdapter={AdapterDayjs}>
             <GlobalStyles>
                 <App />
             </GlobalStyles>
             <ToastContainer position="bottom-right" theme="colored" />
         </LocalizationProvider>
-    </BrowserRouter>,
-);
+      </RouterProvider>
+    </React.StrictMode>
+  );
 reportWebVitals();

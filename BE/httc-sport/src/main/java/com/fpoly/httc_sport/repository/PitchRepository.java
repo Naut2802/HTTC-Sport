7fc937f11b2774a8f1ce0880634ff4efb35e7fcc
Naut2802 <willbin012@gmail.com>
2024-08-26 09:50:14 +0700

    new feature: get top 3 most rented pitches

--- BE/httc-sport/src/main/java/com/fpoly/httc_sport/repository/PitchRepository.java
+++ BE/httc-sport/src/main/java/com/fpoly/httc_sport/repository/PitchRepository.java
@@ -14,10 +14,10 @@ import java.util.List;
 public interface PitchRepository extends JpaRepository<Pitch, Long>, JpaSpecificationExecutor<Pitch> {
 	Page<Pitch> findAllByIsEnabledTrue(Pageable pageable);
 	
-	@Query("SELECT new com.fpoly.httc_sport.dto.response.PitchResponse(b.pitch.id, b.pitch.pitchName, b.pitch.price, " +
+	@Query(value = "SELECT new com.fpoly.httc_sport.dto.response.PitchResponse(b.pitch.id, b.pitch.pitchName, b.pitch.price, " +
 			"b.pitch.address.street, b.pitch.address.ward, b.pitch.address.district, b.pitch.address.city, " +
 			"b.pitch.description, b.pitch.isEnabled, b.pitch.type, b.pitch.total, " +
 			"(SELECT img.url FROM Image img WHERE img.pitch.id = b.pitch.id ORDER BY img.id ASC LIMIT 1), b.pitch.rate) " +
-			"FROM Bill b GROUP BY b.pitch.id ORDER BY COUNT(b) DESC")
+			"FROM Bill b GROUP BY b.pitch.id ORDER BY COUNT(b) DESC", nativeQuery = true)
 	List<PitchResponse> findTop3MostRentedPitch(Pageable pageable);
 }
